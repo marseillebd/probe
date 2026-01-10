76d55f992c06329c78921cff2116b6ad9deddb3d
Marseille Bouchard <marseillebd@proton.me>
2026-01-10 15:58:25 -0500

    trying out inotify form bash

--- envs/shell/run.bash
+++ envs/shell/run.bash
@@ -5,7 +5,41 @@ shopt -s globstar
 ### Main ###
 
 main() {
-  echo hello
+  local input="input.txt"
+
+  echo >&2 "=== regenerating input file ==="
+  echo | tee "$input" <<EOF
+Hello, world!
+Hello, user!"
+42 137 42
+EOF
+
+  echo >&2 "=== initial indexing ==="
+  reindex "$input" | tee "$input.index-words"
+
+  echo >&2 "=== setup inotify ==="
+  ( inotifywait -e modify "$input" >&2
+    echo >&2 "=== reindexing ==="
+    cp "$input.index-words" "$input.index-words.bak"
+    reindex "$input" | tee "$input.index-words"
+    echo >&2 "=== the resulting diff ==="
+    icdiff "$input.index-words"{.bak,}
+  ) &
+
+  echo >&2 "=== editing input file ==="
+  sleep 1
+  echo "foo world" >> "$input"
+
+  wait
+}
+
+reindex() {
+  if [[ $# != 1 ]]; then
+    echo >&2 "$0 - internal error: usage: reindex <filepath>"
+    return 1
+  fi
+  local f="$1"
+  <"$f" grep -ioP '[a-z]+|[0-9]+' | sort -u
 }
 
 ###################
---
+++ learn/fsnotify/run.bash
@@ -0,0 +1,1 @@
+../../envs/shell/run.bash
