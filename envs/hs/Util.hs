64209d6e7dd1de554b570ac55149dd3ea7a7745e
Marseille Bouchard Demko <marseillebd@proton.me>
2026-02-01 05:57:47 -0500

    a handful of hs utilities, as I need them

--- envs/hs/Util.hs
+++ envs/hs/Util.hs
@@ -1,18 +1,34 @@
 module Util
   ( module X
+  -- Hardened &/ Generalized Prelude
   , head, unsafeHead
+  -- * Control Flow
+  , maybeM_, forM, forM_
+  -- * Shell-type Shii
+  , mkdir, mkdir_p
+  , (</>), (<.>), (-<.>)
   ) where
 
 import Prelude as X hiding (head)
 import qualified Prelude
 
+import System.Directory
+
+import Control.Monad (void, forM, forM_)
 import Data.ByteString (ByteString)
 import Data.Map (Map)
 import Data.Text (Text)
+import System.FilePath ((</>), (<.>), (-<.>))
 
 head :: [a] -> Maybe a
 head [] = Nothing
 head (x:_) = Just x
 
-unsafeHead :: [a] -> a
 unsafeHead = Prelude.head
+
+maybeM_ :: (Applicative m) => Maybe a -> (a -> m x) -> m ()
+maybeM_ Nothing _ = pure ()
+maybeM_ (Just x) action = void $ action x
+
+mkdir = createDirectory
+mkdir_p = createDirectoryIfMissing True
