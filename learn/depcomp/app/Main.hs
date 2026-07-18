2b8b1c5944e94f46a7e0d95e9490eae8b06ec819
Marseille Bouchard <marseillebd@proton.me>
2026-07-18 16:24:45 -0400

    gensym in my toy compiler

--- learn/depcomp/app/Main.hs
+++ learn/depcomp/app/Main.hs
@@ -1,9 +1,17 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Main (main) where
 
 import Text.Read (readMaybe)
 import System.Environment (getArgs)
 import System.IO (hPutStrLn, stderr)
 import System.Exit (exitFailure)
+import Data.Map (Map)
+import Control.Monad.State (State, gets, modify, evalState)
+import Data.Maybe (fromMaybe)
+
+import qualified Data.Map as Map
 
 main :: IO ()
 main = do
@@ -12,27 +20,57 @@ main = do
   ast <- case parseFile fileContent of
     Nothing -> die "syntax error"
     Just it -> pure it
-  putStr $ compileProgram ast
+  putStr $ runQbe $ compileProgram ast
 
 ---------------------
 ------ Compile ------
 ---------------------
 
-compileProgram :: Syntax -> String
+compileProgram :: Syntax -> Qbe String
 compileProgram ast = compileFunction "main" ast
 
-compileFunction :: String -> Syntax -> String
-compileFunction funcName (IntLit n) =
-  "export function w $"++ funcName ++ "() {\n\
-  \  @start\n\
-  \  %v0 =w copy " ++ show n ++ "\n\
-  \  ret %v0\n\
-  \}\n\
-  \"
+compileFunction :: String -> Syntax -> Qbe String
+-- FIXME we need to gensym instead of hardcoding eg %v0
+compileFunction funcName (IntLit n) = do
+  v <- gensym "v"
+  pure $ unlines
+    [ "export function w $"++ funcName ++ "() {"
+    , "  @start"
+    , "  %" ++ v ++ " =w copy " ++ show n ++ ""
+    , "  ret %" ++ v
+    , "}"
+    ]
+
+newtype Qbe a = Qbe { unQbe :: State QbeSt a }
+  deriving (Functor, Applicative, Monad)
+data QbeSt = QbeSt
+  { varNames :: Map String Int
+  }
+runQbe :: Qbe a -> a
+runQbe (Qbe action) = evalState action st0
+  where
+  st0 = QbeSt
+    { varNames = Map.empty
+    }
+
+
+gensym :: String -> Qbe String
+gensym baseName = Qbe $ do
+  nextInt <- gets $ fromMaybe 0 . Map.lookup baseName . varNames
+  modify $ \st -> st { varNames = Map.insert baseName (nextInt + 1) st.varNames }
+  pure $ baseName ++ show nextInt
 
 --------------------
 ------ Syntax ------
 --------------------
+
+{- TODO
+I'm already thinking about over-abstracting this.
+But to counteract that, how awbout this:
+`Syntax` is just an s-expression, and then we have patterns that match the relevant bits of s-expr.
+I'd still need a Vault, or an annotation (which I'm really thinking about using the `Syntax f = f (Syntax f)` trick) or something.
+Yeah, the annotations are for source location or type annotation or whatever.
+-}
 
 data Syntax
   = IntLit Integer
